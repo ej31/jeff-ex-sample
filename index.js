bb78cd5079e6beb84449615bce962eaab91c1f95
jeff <itj1032@gmail.com>
2024-12-12 23:38:20 +0900

    chore: logging

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const my = require('mysql2')
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const gw = (req, res, next) => {
+    console.log(`[${req.rawHeaders[1]}][${req.method}] ${req.url} `)
+    next()
+}
+
+app.use(gw)
 app.get('/', (req, res) => {
-  res.send('헬로 월드🌸');
+    res.send('헬로 월드🌸');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
--- issue-delete-by-label.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 특정 라벨이 있는 모든 이슈를 closed로 변경
-gh issue list -l "dummy" --json number --jq '.[].number' | while read issue_number; do
-    gh issue close $issue_number
-    echo "Closed issue #$issue_number"
-done
